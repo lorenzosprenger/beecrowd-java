c6282075c2eef96943be13679c5dd30f5c511203
lorenzovanlaresprenger <lorenzosprenger@gmail.com>
2026-08-14 11:12:25 -0300

    1050

---
+++ src/bee1050/Main.java
@@ -0,0 +1,25 @@
+package bee1050;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int x = sc.nextInt();
+
+        switch (x) {
+            case 61 -> System.out.println("Brasilia");
+            case 71 -> System.out.println("Salvador");
+            case 11 -> System.out.println("Sao Paulo");
+            case 21 -> System.out.println("Rio de Janeiro");
+            case 32 -> System.out.println("Juiz de Fora");
+            case 19 -> System.out.println("Campinas");
+            case 27 -> System.out.println("Vitoria");
+            case 31 -> System.out.println("Belo Horizonte");
+            default -> System.out.println("DDD nao cadastrado");
+        }
+
+        sc.close();
+    }
+}
