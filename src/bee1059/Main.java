911854c771c74239ca43f2d48620decdcfa0d08d
lorenzovanlaresprenger <lorenzosprenger@gmail.com>
2026-08-20 13:24:49 -0300

    1059

---
+++ src/bee1059/Main.java
@@ -0,0 +1,12 @@
+package bee1059;
+
+public class Main {
+    public static void main(String[] args) {
+        for (int i = 2; i < 101; i++) {
+            if (i % 2 == 0){
+                System.out.println(i);
+            }
+
+        }
+    }
+}
