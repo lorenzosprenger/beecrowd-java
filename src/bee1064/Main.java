4a2eb3d9fac5aab78c26ec4d904ab4b62d8ee409
lorenzovanlaresprenger <lorenzosprenger@gmail.com>
2026-08-26 11:19:49 -0300

    1064

---
+++ src/bee1064/Main.java
@@ -0,0 +1,28 @@
+package bee1064;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+
+
+        int contador = 0;
+        double media = 0;
+
+        for (int i = 1; i <= 6; i++) {
+            double numero =  sc.nextDouble();
+            if  (numero >= 0) {
+                contador++;
+                media += numero;
+            }
+
+        }
+
+        media = media / contador;
+        System.out.println(contador + " valores positivos");
+        System.out.printf("%.1f", media);
+    }
+}
