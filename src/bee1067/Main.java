b981fb773c5f5d64ead66f677bbfc1d15df0d409
lorenzovanlaresprenger <lorenzosprenger@gmail.com>
2026-08-27 15:10:56 -0300

    1067

---
+++ src/bee1067/Main.java
@@ -0,0 +1,17 @@
+package bee1067;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int n = input.nextInt();
+
+        for (int i = 0; i < n; i++) {
+            if (i % 2 == 1) {
+                System.out.println(i);
+            }
+
+        }
+    }
+}
